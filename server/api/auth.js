65a8d19236bde84ba5cf31c55dd03ab5d1b821f1
Hector <nokure@gmail.com>
2023-09-03 17:29:26 +1000

    First registration method attempt.

--- server/api/auth.js
+++ server/api/auth.js
@@ -25,4 +25,39 @@ router.post('/', (req, res) => {
     }
 });
 
+/**
+ * POST /api/auth/register
+ * Register a new user
+ * @param {string} username
+ * @param {string} email
+ * @param {string} password
+ * @returns {object} user
+ */
+router.post('/register', (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Check if the email or username is already in use
+    const existingUser = users.find(u => u.email === email || u.username === username);
+
+    if (existingUser) {
+        return res.status(400).json({ message: 'Username or Email already in use' });
+    }
+
+    // Create a new user object
+    const newUser = {
+        id: users.length + 1, // Simple way to auto-increment IDs
+        username,
+        email,
+        password // Note: Store hashed passwords in a production application
+    };
+
+    // Add the new user to the users array
+    users.push(newUser);
+
+    const { password: _, ...userWithoutPassword } = newUser; // Destructure to remove password
+
+    // Return the newly created user
+    return res.status(201).json(userWithoutPassword);
+});
+
 module.exports = router;
